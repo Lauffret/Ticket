fade94d9f7e04a43bd478ac4625670a8195a2066
Lauffret <auffretlea@orange.fr>
2023-08-11 13:08:51 +0200

    projet final

---
+++ ticket-webapp/org/exemple/demo/ticket/webapp/MaClasse.java
@@ -0,0 +1,13 @@
+package org.exemple.demo.ticket.webapp;
+
+public class MaClasse {
+	/**
+     * Méhode d'entrée du programme (main).
+     *
+     * @param pArgs : Les paramètres passés en entrée du programme
+     */
+    public static void main(String[] pArgs) {
+        System.out.println("Hello world !");
+        MaClasse.methodeX();
+    }
+}
